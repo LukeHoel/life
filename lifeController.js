50e0997b66c3e677574e50d92666b647f531625b
Luke Hoel <32398032+LukeHoel@users.noreply.github.com>
2018-11-02 22:46:43 -0400

    Update lifeController.js

--- lifeController.js
+++ lifeController.js
@@ -35,6 +35,18 @@ myApp.controller('lifeController', ['$scope', '$interval', function($scope, $int
         {x:1,y:2},
         {x:2,y:2}
       ]
+    },
+    {
+      name: "Small Exploder",
+      cells:[
+        {x:1,y:0},
+        {x:0,y:1},
+        {x:1,y:1},
+        {x:2,y:1},
+        {x:0,y:2},
+        {x:2,y:2},
+        {x:1,y:3}
+      ]
     }
   ];
 
